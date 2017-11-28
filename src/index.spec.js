b410042fe0196c974f907841a15bd1904f99353f
Fredrik Palmquist <fiddep@telia.com>
2017-11-28 10:49:08 +0100

    Support for eslint and flow at the same time

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,10 +1,7 @@
-import foo from './index';
+import foo from './index'
 
 describe('Write a fun test', () => {
   it('Returns a value', () => {
-    expect(foo()).toEqual('foo');
-  });
-  it('Fails', () => {
-    fail();
-  });
-});
+    expect(foo()).toEqual('foo')
+  })
+})
